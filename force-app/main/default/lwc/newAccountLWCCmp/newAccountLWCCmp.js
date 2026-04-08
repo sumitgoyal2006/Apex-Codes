f0e63690a03ee1f41528e7b47c3909020b2ce994
Sumit Goyal <sumitgoyal4@gmail.com>
2026-04-08 22:55:42 +0530

    Update newAccountLWCCmp.js

--- force-app/main/default/lwc/newAccountLWCCmp/newAccountLWCCmp.js
+++ force-app/main/default/lwc/newAccountLWCCmp/newAccountLWCCmp.js
@@ -1,5 +1,6 @@
 import { LightningElement } from 'lwc';
 import addAccount from '@salesforce/apex/LightningApexClass.addAccount';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class NewAccountLWCCmp extends LightningElement {
     //properties and methods for the component will go here
@@ -23,10 +24,22 @@ export default class NewAccountLWCCmp extends LightningElement {
                     accName:this.accName,
                     accPhone:this.accPhone})
                     .then(result=>{
-                        alert(result);
+                       // alert(result);
+                       const evt=new ShowToastEvent({
+                        title:'Success',
+                        message:result,
+                        variant:'success'
+                       });
+                       this.dispatchEvent(evt);
                     })
                     .catch(error=>{
-                        alert('Failed to Insert Record');
+                       // alert('Failed to Insert Record');
+                        const evt=new ShowToastEvent({
+                        title:'Failed',
+                        message:'Failed to add Account',
+                        variant:'error'
+                       });
+                       this.dispatchEvent(evt);
                     });
     }
 
